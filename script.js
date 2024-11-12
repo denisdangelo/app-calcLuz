fd49c1a23dd7ed401a19ea1bd494c3940ffb0268
denisdangelo <denis.dsilveira1@outlook.com>
2024-11-12 15:53:39 -0300

    App Versão 1

---
+++ script.js
@@ -0,0 +1,31 @@
+/**
+ * Calculo de valor da conta de luz
+ * @author Denis D'Angelo
+ */
+
+//biblioteca
+/*const input = require('readline-sync')
+const colors = require('colors')
+let cons, klwH, total
+
+console.clear()
+console.log("Valor da conta de luz com base em KwH")
+
+//entrada
+klwH = Number(input.question("Insira o valor da tarifa: R$ "))
+cons = Number(input.question("Insira o consumo nos últimos 30 dias em Klw: "))
+
+//processamento
+total = cons * klwH
+
+//saida
+console.log(`sua conta será R${total.toFixed(2)}`)
+*/
+
+function calcular(event){
+	event.preventDefault()
+	let consumo = document.getElementById('vcons').value
+	let kilowatts = document.getElementById('vklwH').value 
+	console.log(consumo)
+	console.log(kilowatts)
+}
